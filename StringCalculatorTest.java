5787c33b263d37b9aaaeffb6a319c4372b8e08b1
athiraknair743 <athirakookkal@gmail.com>
2025-09-22 22:58:55 +0530

    Update StringCalculatorTest.java

--- StringCalculatorTest.java
+++ StringCalculatorTest.java
@@ -1,1 +1,12 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringCalculatorTest {
+
+    @Test
+    void shouldReturnZeroForEmptyString() {
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(0, calculator.add(""));
+    }
+}
 
