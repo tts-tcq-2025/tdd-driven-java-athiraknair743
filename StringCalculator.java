fe681a37b863fade39f8ad35b5638cb5ada2ac5c
athiraknair743 <athirakookkal@gmail.com>
2025-09-22 23:10:40 +0530

    Update StringCalculator.java

--- StringCalculator.java
+++ StringCalculator.java
@@ -1,6 +1,10 @@
-public class StringCalculator {
+public int add(String input) {
+    if (input.isEmpty()) return 0;
 
-    public int add(String input) {
-        return 0;
+    String[] numbers = input.split(",");
+    int sum = 0;
+    for (String number : numbers) {
+        sum += Integer.parseInt(number);
     }
+    return sum;
 }
